4213d136f72254c8ed31a1b5078a1fa9dd4c3d42
engineman11 <111449901+engineman11@users.noreply.github.com>
2025-07-15 23:39:59 +0300

    add variable jump height

--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -220.0
+const JUMP_VELOCITY = -250.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,12 +12,15 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity / 2 * delta
+	
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_released("jump") and velocity.y < 0.0:
+		velocity.y = 0
+	elif not is_on_floor():
+		velocity.y += gravity / 2 * delta
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
